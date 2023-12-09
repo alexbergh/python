ff5002fb98297700e13d36c6c3fd9e737c930862
ALEX HELLBERG <81573329+alexbergh@users.noreply.github.com>
2023-12-09 21:23:28 +0500

    Update fb_to_xml.py

--- fb_to_xml.py
+++ fb_to_xml.py
@@ -1,7 +1,6 @@
 import firebirdsql
 import xml.etree.ElementTree as ET
 
-# Connecting to a Firebird database
 firebird_conn = firebirdsql.connect(
     host='ip adress',
     user='db username',
@@ -9,17 +8,13 @@ firebird_conn = firebirdsql.connect(
     database='name bd'
 )
 
-# Creating an object to execute SQL queries
 firebird_cursor = firebird_conn.cursor()
 
-# Running an SQL query to retrieve data from Firebird
 firebird_query = "SELECT * FROM table_used"
 firebird_cursor.execute(firebird_query)
 
-# Creating an xml root element
 root = ET.Element("data")
 
-# Extracting data from a query result and creating xml elements
 for row in firebird_cursor.fetchall():
     item = ET.Element("item")
     root.append(item)
@@ -28,11 +23,9 @@ for row in firebird_cursor.fetchall():
         field.text = str(field_value)
         item.append(field)
 
-# Creating an xml document and writing it to a file
 xml_data = ET.ElementTree(root)
 xml_data.write("output.xml")
 
-# Closing a connection to a Firebird database
 firebird_cursor.close()
 firebird_conn.close()
 
